6b036ad4b120dfc3e044a827ce1bad1dde7308db
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-09-20 13:39:21 -0300

    - fixed hilt crashing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-text-google-fonts:1.6.8")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation( "com.google.dagger:hilt-android:2.44")
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
