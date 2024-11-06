a8229040a9cd10e1a8c5295de0ee8858526527f9
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-11-06 11:46:06 -0300

    - border color of calendar

--- app/src/main/java/com/example/diseomobile/Components/Calendar/Calendar.kt
+++ app/src/main/java/com/example/diseomobile/Components/Calendar/Calendar.kt
@@ -1,6 +1,7 @@
 package com.example.diseomobile.Components.Calendar
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,8 +15,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.diseomobile.ui.theme.Tertiary400
+import com.example.diseomobile.ui.theme.mediumBorder
 import com.example.diseomobile.ui.theme.mediumLargePadding
 import com.example.diseomobile.ui.theme.roundedCorners
+import com.example.diseomobile.ui.theme.smallBorder
 import com.example.diseomobile.ui.theme.smallDP
 import java.util.Date
 
@@ -24,7 +27,7 @@ fun CalendarComposable(selectedWeekDate : (List<Date>) -> Unit = {}, closeCalend
 
     Box(modifier = Modifier
         .padding(mediumLargePadding)
-        .shadow(elevation = smallDP, shape = RoundedCornerShape(roundedCorners))
+        .border(width = mediumBorder, color = MaterialTheme.colorScheme.primary ,shape = RoundedCornerShape(roundedCorners))
         .background(color = MaterialTheme.colorScheme.background, shape = RoundedCornerShape(roundedCorners)))
     {
         MonthSelector(selectedWeekDate, closeCalendar)
